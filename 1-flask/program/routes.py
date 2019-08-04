c232981d94618ad481243e6369450b62c329b22e
vindard <17693119+vindard@users.noreply.github.com>
2019-08-04 15:05:40 -0400

    add html rendering

--- 1-flask/program/routes.py
+++ 1-flask/program/routes.py
@@ -1,7 +1,8 @@
 from program import app
+from flask import render_template
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hellow Pythonistas"
+    return render_template('index.html')
 
